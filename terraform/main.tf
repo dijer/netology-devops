829af144e4c87b36cc0591ec30c69f468d715313
Timur Ahmetzyanov <dijer@mail.ru>
2022-07-02 16:59:14 +0300

    07-terraform-03-basic homework

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,11 @@ provider "yandex" {
 }
 
 resource "yandex_compute_instance" "vm" {
+  count = 2
   name = "ubuntu"
   zone = "ru-central1-a"
   resources {
-    cores = 2
+    cores = locals.cores[terraform.workspace]
     memory = 2
   }
   boot_disk {
@@ -22,5 +23,40 @@ resource "yandex_compute_instance" "vm" {
   }
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm2" {
+  for_each = toset([ "stage", "prod" ])
+  name = "ubuntu"
+  zone = "ru-central1-a"
+  resources {
+    cores = locals.cores[each.key]
+    memory = 2
+  }
+  boot_disk {
+    initialize_params {
+      image_id = var.yc_compute_image_id
+    }
+  }
+  network_interface {
+    subnet_id = var.subnet_id
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+locals {
+  cores = {
+    stage = 2
+    prod = 4
+  }
+  count = {
+    stage = 1
+    prod = 2
   }
 }
